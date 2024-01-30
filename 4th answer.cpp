48602b3a86257487e6096dc1448f9e6a026a4f5a
divyaboppudi <157440232+divyaboppudi@users.noreply.github.com>
2024-01-29 19:10:00 -0500

    Update 3rd answer.cpp

--- 4th answer.cpp
+++ 4th answer.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
-
-// An optimized version of Bubble Sort
-void bubbleSort(int arr[], int n) {
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements using temporary variable
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = true;
-            }
+using namespace std;
+int main(){
+    
+int a[]={24,12,35,23,45,34,20,48};
+int getArrayLength = sizeof(a) / sizeof(int);
+for (int i = 0; i < getArrayLength; i++)//up unitl the length
+{
+    for(int j = 0;j<getArrayLength-1; j++){//up until last but one
+        if(a[j]>a[j+1]){
+            int temp = a[j];
+            a[j] = a[j+1];
+            a[j+1] = temp;
         }
-
-        // If no two elements were swapped
-        // by the inner loop, then break
-        if (!swapped)
-            break;
     }
 }
-
-// Function to print an array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        std::cout << " " << arr[i];
+//printing
+for (int i=0;i<getArrayLength;i++){
+    cout<<a[i];
 }
-
-// Driver program to test above functions
-int main() {
-    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int N = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, N);
-    std::cout << "Sorted array:\n";
-    printArray(arr, N);
-    return 0;
 }
